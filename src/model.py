319b15271ff8856ff7e3ba40caf0bcf53f84fbba
GuoQi2000 <97843039+GuoQi2000@users.noreply.github.com>
2024-07-23 17:37:25 +0800

    more stable API requests

--- src/model.py
+++ src/model.py
@@ -34,45 +34,32 @@ class APIModel:
                     return json.loads(response.text)['choices'][0]['message']['content']
                 except:
                     pass
+                time.sleep(0.2)
             return None
     
-    # def req(self, text, temperature=1):
-    #     url = f"{self.__api_url}"
-    #     pay_load_dict = {"model": f"{self.model}","messages": [{
-    #             "role": "user",
-    #             "temperature":temperature,
-    #             "content": f"{text}"}]}
-    #     payload = json.dumps(pay_load_dict)
-    #     headers = {
-    #     'Accept': 'application/json',
-    #     'Authorization': f'Bearer {self.__api_key}',
-    #     'User-Agent': 'Apifox/1.0.0 (https://apifox.com)',
-    #     'Content-Type': 'application/json'
-    #     }
-        
-    #     response = requests.request("POST", url, headers=headers, data=payload)
-    #     print(response)
-    #     print(response.text)
-    #     return json.loads(response.text)['choices'][0]['message']['content']
-
     def chat(self, text, temperature=1):
         response = self.__req(text, temperature=temperature, max_try=5)
         return response
 
     def __chat(self, text, temperature, res_l, idx):
-
+        
         response = self.__req(text, temperature=temperature)
         res_l[idx] = response
         return response
         
     def batch_chat(self, text_batch, temperature=0):
+        max_threads=15 # limit max concurrent threads using model API
         res_l = ['No response'] * len(text_batch)
         thread_l = []
         for i, text in zip(range(len(text_batch)), text_batch):
             thread = threading.Thread(target=self.__chat, args=(text, temperature, res_l, i))
             thread_l.append(thread)
             thread.start()
-            time.sleep(0.2)
+            while len(thread_l) >= max_threads: 
+                for t in thread_l:
+                    if not t .is_alive():
+                        thread_l.remove(t)
+                time.sleep(0.3) # Short delay to avoid busy-waiting
 
         for thread in tqdm(thread_l):
             thread.join()
